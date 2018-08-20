e301e30f6e38213b4e43ac86498af6da31d6fa7b
Dave Stearns <dlsinfo@uw.edu>
2018-08-19 17:10:59 -0700

    rename project to userservice, and fix prob with unexported config fields

--- main.go
+++ main.go
@@ -7,15 +7,15 @@ import (
 
 	"github.com/caarlos0/env"
 	"github.com/davestearns/sessions"
-	"github.com/davestearns/users/handlers"
+	"github.com/davestearns/userservice/handlers"
 )
 
 type config struct {
-	addr          string   `env:"ADDR" envDefault:":80"`
-	redisAddr     string   `env:"REDIS_ADDR" envDefault:"127.0.0.1:6379"`
-	sessionKeys   []string `env:"SESSION_KEYS,required"`
-	dynamoDBTable string   `env:"DYNAMODB_TABLE" envDefault:"users"`
-	dynamoDBKey   string   `env:"DYNAMODB_KEY" envDefault:"userName"`
+	Addr          string   `env:"ADDR" envDefault:":80"`
+	RedisAddr     string   `env:"REDIS_ADDR" envDefault:"127.0.0.1:6379"`
+	SessionKeys   []string `env:"SESSION_KEYS,required"`
+	DynamoDBTable string   `env:"DYNAMODB_TABLE" envDefault:"users"`
+	DynamoDBKey   string   `env:"DYNAMODB_KEY" envDefault:"userName"`
 }
 
 func main() {
@@ -25,16 +25,15 @@ func main() {
 	}
 	log.Printf("using the following configuration: %+v", cfg)
 
+	sessionStore := sessions.NewRedisStore(sessions.NewRedisPool(cfg.RedisAddr, time.Minute*10), time.Hour)
 	handlerConfig := &handlers.Config{
-		Manager: sessions.NewManager(sessions.DefaultIDLength,
-			cfg.sessionKeys,
-			sessions.NewRedisStore(sessions.NewRedisPool(cfg.redisAddr, time.Minute*10), time.Hour)),
+		Manager: sessions.NewManager(sessions.DefaultIDLength, cfg.SessionKeys, sessionStore),
 	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/users", handlerConfig.UsersHandler)
 
-	log.Printf("server is listening at http://%s...", cfg.addr)
-	log.Fatal(http.ListenAndServe(cfg.addr, mux))
+	log.Printf("server is listening at http://%s...", cfg.Addr)
+	log.Fatal(http.ListenAndServe(cfg.Addr, mux))
 
 }
